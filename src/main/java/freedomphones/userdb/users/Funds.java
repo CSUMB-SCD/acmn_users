bb1369a137b749456d132b49d15e5241c983398a
cforsythe <cforsythe@csumb.edu>
2018-12-01 15:26:33 -0800

    added funds

---
+++ src/main/java/freedomphones/userdb/users/Funds.java
@@ -0,0 +1,16 @@
+package freedomphones.userdb.users;
+
+public class Funds{
+    private Float funds;
+
+    public Funds(float funds){
+        this.funds = funds;
+    }
+
+    public void setFunds(Float funds){
+        this.funds = funds;
+    }
+    public Float getFunds(){
+        return this.funds;
+    }
+}
